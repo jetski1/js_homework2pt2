e26b3f27a6311da1a83149b11dc2b1343b2880bc
Bekzod <Bekzod@gmailc.com>
2020-12-28 23:15:43 +0500

    FINAL

---
+++ script.js
@@ -0,0 +1,50 @@
+var time = prompt('Введите время')
+if(time == 0 ) {
+alert(" 12 часов ночи ")
+} else if(time == 1 ) {
+    alert(' Час ночи  ')
+} else if(time == 2) {
+    alert('Два часа ночи')
+} else if(time == 3) {
+    alert('Три часа ночи')
+} else if(time == 4) {
+    alert('Четыре часов утра')
+} else if(time == 5) {
+    alert('Пять часов утра   ' )
+} else if(time == 6) {
+    alert('Шесть часов утра   ' )
+}else if(time == 7) {
+    alert('Семь часов утра   ' )
+}else if(time == 8) {
+    alert('Восемь часов утра   ' )
+}else if(time == 9) {
+    alert('9 часов утра   ' )
+}else if(time == 10) {
+    alert('10 часов утра   ' )
+}else if(time == 11) {
+    alert('11 часов утра   ' )
+}else if(time == 12) {
+    alert('12 часов дня   ' )
+}else if(time == 13) {
+    alert('Обед ' )
+}else if(time == 14) {
+    alert('2 часа дня  ' )
+}else if(time == 15) {
+    alert('3 часа дня  ' )
+}else if(time == 16) {
+    alert('Полдник' )
+}else if(time == 17) {
+    alert(' 5 часов ночи  ' )
+}else if(time == 18) {
+    alert(' 6 часов ночи  ' )
+}else if(time == 19) {
+    alert(' 7 часов ночи  ' )
+}else if(time == 20) {
+    alert(' 8 часов ночи  ' )
+}else if(time == 21) {
+    alert(' 9 часов ночи  ' )
+}else if(time == 22) {
+    alert(' 10 часов ночи  ' )
+}else if(time == 23) {
+    alert(' 11 часов ночи  ' )
+}
